901cf4cbeb1b7bce27f0defad913fc96917c7d3b
Unit One ICT <m.sh@unitone.ps>
2015-04-15 19:37:55 +0300

    Login By Facebook!
    Using FacebookRedirectLoginHelper

--- index.php
+++ index.php
@@ -5,21 +5,33 @@
 
 	// Load the Facebook classes
 	use Facebook\FacebookSession;
-	use Facebook\FacebookRequest;
-	use Facebook\GraphPage;
+	use Facebook\FacebookRedirectLoginHelper;
 	
 	// Set the initial Facebook App ID and Secret key
 	// get your App ID and Secret from here (https://developers.facebook.com/apps/)
 	$App_ID 	= "XXX";
 	$App_Secret = "YYY";
 	FacebookSession::setDefaultApplication($App_ID, $App_Secret);
-
-	// Get the Application level Access Token
-	$session = FacebookSession::newAppSession();
 	
-	// Using Graph API to get a Page information
-	$page = (new FacebookRequest(
-		$session, 'GET', '/Jawwal.059'
-	))->execute()->getGraphObject(GraphPage::className());
-	// Output page name.
-	echo $page->getName();
+	// login helper with redirect_uri
+	$helper = new FacebookRedirectLoginHelper( 'http://localhost/facebook/' );
+
+	try {
+		// Get access token from the code parameter in the URL.
+		$session = $helper->getSessionFromRedirect();
+	} catch( FacebookRequestException $ex ) {
+		// When Facebook returns an error.
+		$session = null;
+	} catch( \Exception $ex ) {
+		// When validation fails or other local issues.
+		$session = null;
+	}
+
+	if ( isset( $session ) ) {
+		// Successfully got a valid session for the user
+		echo "Logged In";
+	} else {
+		// Generate the login URL for Facebook authentication.
+		$loginUrl = $helper->getLoginUrl();
+		echo '<a href="' . $loginUrl . '">Login</a>';
+	}
